92f803490799de7a719ff028216635f02bfe0cc9
LasAntti <antti.lassila98@gmail.com>
2024-02-06 20:08:55 +0200

    Testattu API-uutisia varten

--- app/src/main/java/com/example/shortnews/pages/HomePageActivity.kt
+++ app/src/main/java/com/example/shortnews/pages/HomePageActivity.kt
@@ -24,8 +24,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -33,6 +35,9 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shortnews.ui.theme.ShortNewsTheme
+import com.example.shortnews.utility.NewsApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class HomePageActivity : ComponentActivity() {
@@ -73,6 +78,14 @@ val items = listOf(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage() {
+
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(true) {
+        scope.launch(Dispatchers.IO) {
+            NewsApiService.fetchNews()
+        }
+    }
 
     var selectedItem by rememberSaveable {
         mutableIntStateOf(0)
@@ -128,7 +141,6 @@ fun HomePage() {
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
