de9d30febe335f01e53fc1ad00605c89bb7cfa12
LasAntti <antti.lassila98@gmail.com>
2024-02-17 16:06:02 +0200

    Retrieve news from API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,10 @@ dependencies {
     implementation ("androidx.compose.foundation:foundation:1.6.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.moshi:moshi:1.15.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.5.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
